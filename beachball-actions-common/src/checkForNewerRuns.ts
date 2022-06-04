cafc8f3a2005427f98c302bbeaf3c469d994b1e9
Elizabeth Craig <elcraig@microsoft.com>
2022-06-03 21:32:52 -0700

    try another way to get workflow runs

--- beachball-actions-common/src/checkForNewerRuns.ts
+++ beachball-actions-common/src/checkForNewerRuns.ts
@@ -16,17 +16,31 @@ export async function checkForNewerRuns(): Promise<boolean> {
   const octokit = getOctokit();
 
   const branchName = process.env.GITHUB_REF_NAME!;
-  const workflowId = process.env.GITHUB_WORKFLOW!;
+  const runId = Number(process.env.GITHUB_RUN_ID!);
+  let workflowId: number;
+
+  try {
+    workflowId = (
+      await octokit.rest.actions.getWorkflowRun({
+        ...github.context.repo,
+        run_id: runId,
+      })
+    ).data.workflow_id;
+  } catch (err) {
+    logGithubRequestError(err, `info about workflow run "${runId}"`);
+    process.exit(1);
+  }
 
   let thisBranchRunCount: number;
   try {
-    const result = await octokit.rest.actions.listWorkflowRuns({
-      ...github.context.repo,
-      workflow_id: workflowId,
-      status: 'pending' as any, // eslint-disable-line -- "pending" is valid but missing from types
-      branch: branchName,
-    });
-    thisBranchRunCount = result.data.total_count;
+    thisBranchRunCount = (
+      await octokit.rest.actions.listWorkflowRuns({
+        ...github.context.repo,
+        workflow_id: workflowId,
+        status: 'pending' as any, // eslint-disable-line -- "pending" is valid but missing from types
+        branch: branchName,
+      })
+    ).data.total_count;
   } catch (err) {
     logGithubRequestError(err, `runs of workflow "${workflowId}" for branch "${branchName}"`);
     process.exit(1);
