80f3f554f7243e65afef4f95ffd4d45f4269ee71
Elizabeth Craig <elcraig@microsoft.com>
2022-06-03 17:47:22 -0700

    Revert "hack to test actions within repo"
    This reverts commit fa710e4ad9838a83ba59e0c1277a6a59768a071a.

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// Hack to help with testing actions within this repo...
-// For JS actions, the "main" will be interpreted relative to the repo root,
-// not relative to the action.yaml. This file translates.
-if (process.env.GITHUB_ACTION) {
-  console.log(process.env.GITHUB_ACTION);
-}
