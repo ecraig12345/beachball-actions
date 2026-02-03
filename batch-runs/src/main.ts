b01ab0c05b19578f801a34b66d0c39a3556605b2
Elizabeth Craig <ecraig12345@gmail.com>
2026-02-03 10:57:24 -0800

    Add deprecation warnings (#1)

--- batch-runs/src/main.ts
+++ batch-runs/src/main.ts
@@ -7,6 +7,10 @@ import {
 } from 'beachball-actions-common';
 
 const main = async (): Promise<void> => {
+  core.warning(
+    'ecraig12345/beachball-actions is deprecated. Please use microsoft/beachball-actions instead.',
+  );
+
   const mode = getEnumInput('mode', ['cancel', 'output'] as const, 'cancel');
   const token = core.getInput('token', { required: true });
 
